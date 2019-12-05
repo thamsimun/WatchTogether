cd2a615013a7e8b087c64bc8a11cf086602f68b7
thamsimun <e0201281@u.nus.edu>
2019-12-06 00:55:24 +0800

    fix bugs

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,12 @@
+// var express = require('express');
+// var app = express();
+//
+// app.use(express.static(__dirname + "/public" ));
+//
+// app.get('/',(req,res) => {
+//     res.redirect('index.html');
+// });
+
 // var firebase = require(firebase/)
 //
 // var config = {
